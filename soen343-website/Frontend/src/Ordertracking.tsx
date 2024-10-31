6f4d673916a4ff0dd8f4cf93c8e1a0b9eecae207
george_ezzat <georgeezzat2002@gmail.com>
2024-10-31 12:33:30 -0400

    main page modifications

---
+++ soen343-website/Frontend/src/Ordertracking.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./header"
+
+const Tracking: React.FC = () => {
+    return (
+        <>
+        <Header/>
+        <div>
+            <h1>Order Tracking</h1>
+            <p>Track your order status here.</p>
+            {/* Placeholder for tracking input or order status display */}
+        </div>
+        </>
+    );
+};
+
+export default Tracking;
--- soen343-website/Frontend/src/header.jsx
+++ soen343-website/Frontend/src/header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './header.css';
-import { Link } from 'react-router-dom';
+import { NavLink  } from 'react-router-dom';
 import logo from './Images/Logo.webp';
 
 function Header() {
@@ -17,15 +17,15 @@ function Header() {
       </button>
 
       <div className="logo-title">
-        <Link to="/" className="navTitleName">
+        <NavLink  to="/" className="navTitleName">
           <img src={logo} alt="Logo" />
-        </Link>
-        <span className="site-title">Turbo Trucks</span>
+        </NavLink >
+        <span className="site-title">TurboTrucks</span>
       </div>
 
       <button className="contactus">Contact Us</button>
       
-
+      
       <div className="auth-buttons">
         <button className="LogBtn">Log In</button>
         <button className="SignBtn">Sign In</button>
@@ -34,10 +34,18 @@ function Header() {
       {isOpen && (
         <aside className="aside-menu">
           <ul>
-            <li><Link to='/' className='desktopMenuListItem'>Home</Link></li>
-            <li><Link to="/tracking" className='desktopMenuListItem'>Tracking Your Order</Link></li>
-            <li><Link to="/product_purchase" className='desktopMenuListItem'> Buy a Product</Link></li>
-            <li><Link to="/aboutus" className='desktopMenuListItem'>About Us</Link></li>
+          <li>
+              <NavLink to="/" exact className="desktopMenuListItem" activeClassName="active">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/tracking" className="desktopMenuListItem" activeClassName="active">Tracking Your Order</NavLink>
+            </li>
+            <li>
+              <NavLink to="/product_purchase" className="desktopMenuListItem" activeClassName="active">Buy a Product</NavLink>
+            </li>
+            <li>
+              <NavLink to="/aboutus" className="desktopMenuListItem" activeClassName="active">About Us</NavLink>
+            </li>
           </ul>
         </aside>
       )}
