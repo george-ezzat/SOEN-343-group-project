a788fc5447640fefa3f091e401d406c8c6b0fb25
dellapenta <jonathandellapenta@gmail.com>
2024-11-05 16:58:46 -0500

    added sign in modal for button

---
+++ code-ninjas-app/frontend/src/components/AccountModal/AccountModal.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './AccountModal.css';
+
+const AccountModal = ({ isOpen, onClose }) => {
+  const [email, setEmail] = useState('');
+  
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+  }
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-button" onClick={onClose}>X</button>
+        <p>Sign in or create an account</p>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder='Email'
+            required
+          />
+          <button className='continue-button' type="submit">Continue</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AccountModal;
