562bdd45b7ada9ea7b391490770d8c806616fe4b
george_ezzat <georgeezzat2002@gmail.com>
2024-11-06 21:06:43 -0500

    adding payment option and transaction approved

--- code-ninjas-app/frontend/src/App.js
+++ code-ninjas-app/frontend/src/App.js
@@ -4,6 +4,8 @@ import Home from './components/Home/Home.jsx'
 import Tracking from './components/OrderTracking/OrderTracking.jsx'
 import AboutUs from './components/AboutUs/AboutUs.jsx'
 import Purchase from './components/ProductPurchase/ProductPurchase.jsx'
+import Payment from './components/ProductPurchase/PaymentForm.jsx'
+import TransactionApproved from './components/ProductPurchase/TransactionApproved.js'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path='/aboutus' element={<AboutUs />} />
           <Route path='/tracking' element={<Tracking />} />
           <Route path='/product_purchase' element={<Purchase />} />
+          <Route path='/payment' element={<Payment />} />
+          <Route path='/transactionapproved' element={<TransactionApproved />} />
         </Routes>
       </BrowserRouter>
     </div>
