4b523d53503bfd2e7f1a7987750c47ad39d14d2d
dellapenta <106177229+dellapenta@users.noreply.github.com>
2024-11-23 22:29:10 -0500

    npm install google maps api

--- code-ninjas-app/frontend/src/components/Home/Home.jsx
+++ code-ninjas-app/frontend/src/components/Home/Home.jsx
@@ -4,19 +4,28 @@ import "./Home.css";
 import Header from "../Header/Header";
 
 export default function Home() {
+
   return (
     <>
       <Header />
       <div className='top-background'></div>
       <div className="background"/>
-        <div className="content">
+      <div className="content">
         <h1>Turbo Trucks</h1>
         <div className='delivery-section'>
           <h2>Start your Delivery</h2>
-          <div className='input-container'>
-            <input type="text" placeholder="Pick-up Location" className='input-box'/>
-            <input type="text" placeholder="Drop-off Destination" className='input-box'/>
-          </div>
+            <div className='input-container'>
+                <input
+                  type="text"
+                  placeholder="Pick-up Location"
+                  className='input-box'
+                />
+                <input
+                  type="text"
+                  placeholder="Drop-off Destination"
+                  className='input-box'
+                />
+            </div>
           <Link to="/delivery">
             <button className='SignupBtn'>Continue</button>
           </Link>
@@ -24,4 +33,4 @@ export default function Home() {
       </div>
     </>
   );
-}
+};
