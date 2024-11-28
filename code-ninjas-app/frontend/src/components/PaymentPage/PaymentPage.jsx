07aa5c3d4d38364f20ac2ba9fdc4d231561eb346
george_ezzat <georgeezzat2002@gmail.com>
2024-11-28 13:41:32 -0500

    implementing reset button

--- code-ninjas-app/frontend/src/components/PaymentPage/PaymentPage.jsx
+++ code-ninjas-app/frontend/src/components/PaymentPage/PaymentPage.jsx
@@ -5,6 +5,13 @@ import Header from '../Header/Header';
 import { calculateCost } from '../../models/costCalculator';
 import { addDelivery } from '../../models/deliveryService';
 
+const getCurrentMonthYear = () => {
+  const today = new Date();
+  const month = String(today.getMonth() + 2).padStart(2, "0");
+  const year = today.getFullYear();
+  return `${year}-${month}`;
+};
+
 const PaymentPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -70,6 +77,14 @@ const PaymentPage = () => {
 
   const totalCost = weightCost + dimensionCost + shippingCost + distanceCost;
 
+  const handleReset = () => {
+    setCardNumber("");
+    setCardholderName("");
+    setBillingAddress("");
+    setExpiryDate("");
+    setCVV("");
+  };
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (validateForm()) {
@@ -156,12 +171,18 @@ const PaymentPage = () => {
           <h2>Payment Form</h2>
           <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <label>Total Cost: ${totalCost.toFixed(2)}</label>
+              <label>Amount</label>
+              <input
+                type="text"
+                value={totalCost.toFixed(2)}
+                readOnly
+              />
             </div>
             <div className="form-group">
               <label>Card Number</label>
               <input
                 type="text"
+                maxLength={16}
                 value={cardNumber}
                 onChange={(e) => setCardNumber(e.target.value)}
                 required
@@ -189,21 +210,28 @@ const PaymentPage = () => {
               <label>Expiry Date</label>
               <input
                 type="month"
+                min={getCurrentMonthYear()}
+                placeholder="MM/YY"
+                required
                 value={expiryDate}
                 onChange={(e) => setExpiryDate(e.target.value)}
-                required
               />
             </div>
             <div className="form-group">
               <label>CVV</label>
               <input
                 type="text"
+                maxLength={3}
+                placeholder="555"
                 value={cvv}
                 onChange={(e) => setCVV(e.target.value)}
                 required
               />
             </div>
-            <button type="submit" className="submit-btn">Submit Payment</button>
+            <div className='buttons'>
+              <button type="submit" className="submit-btn">Submit Payment</button>
+              <button type="button" className="reset-btn" onClick={handleReset}>Reset </button>
+            </div>
           </form>
         </div>
     </div>
